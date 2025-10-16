5ad4fe203ed9f53e15b29ef580ba4d7a1119cfe6
Lexes Bj Acuesta <lexesbjacuesta43@gmail.com>
2025-10-16 09:28:54 +0800

    RetailTransactionCalculator

---
+++ Day1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //retailTransactionCalculator
+
+        double itemPrice = 0;
+        double totalCost = 0;
+        double salesTax = 0;
+        double quantity = 0;
+        double subTotal;
+
+        System.out.print("Enter the item price: ");
+        itemPrice = sc.nextDouble();
+
+        System.out.print("Enter the quantity: ");
+        quantity = sc.nextDouble();
+
+        subTotal = itemPrice * quantity;
+        salesTax = subTotal * 0.056;
+
+        totalCost = subTotal + salesTax;
+
+        System.out.printf("%.2f", totalCost);
+
+        sc.close();
+
+    }
+
+}
